dd2c61d2328e0d4f7c2138e0c07d324b967e3019
ARandomGuy9786 <mgminhtet204@gmail.com>
2026-09-21 18:59:08 +0700

    Add skip and xfail tests

---
+++ week_5_class_exercise/test_skips.py
@@ -0,0 +1,12 @@
+import pytest
+import sys
+
+
+@pytest.mark.skip(reason="Feature not implemented yet")
+def test_future_feature():
+    assert False  # would fail, but is skipped
+
+
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="Requires Python 3.8+")
+def test_needs_modern_python():
+    assert True
